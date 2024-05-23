bf5b6c4661d557f59dd23e27b2c854e03fdf1812
willi <williamacosta4333@gmail.com>
2024-05-22 22:18:09 -0400

    Examen Parcial 2 William Acosta

---
+++ Examenp1/Servicio/Logica/ServSucursal.cs
@@ -0,0 +1,40 @@
+﻿using Examenp1.Reposiroty.sucursal;
+using System;
+using System.Collections.Generic;
+
+namespace Examenp1.Servicio.Logica
+{
+    public class ServSucursal
+    {
+        public SucursalRepository RepositorioSucursal;
+
+        public ServSucursal(string connectionString)
+        {
+            RepositorioSucursal = new SucursalRepository(connectionString);
+        }
+
+        public void AgregarSucursal(Sucursal sucursal)
+        {
+            if (ValidarDatos(sucursal))
+                RepositorioSucursal.Add(sucursal);
+            else
+                throw new Exception("Error en la validación de datos, favor corregir.");
+        }
+
+        public void EliminarSucursal(int id)
+        {
+            RepositorioSucursal.Delete(id);
+        }
+
+        public List<Sucursal> ObtenerTodasLasSucursales()
+        {
+            return RepositorioSucursal.List();
+        }
+
+        private bool ValidarDatos(Sucursal sucursal)
+        {
+
+            return true;
+        }
+    }
+}
